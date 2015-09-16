c87faab0afce5f690f9475664c8ee5070529e5c3
oliverswitzer <oliverswitzer@gmail.com>
2015-09-15 21:53:55 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RescuisineOnRails::Application.config.secret_key_base = '84799706f3d8a07df1b9335a1ef9547ab23358bec27943cbb132c611f0a49eecb5f3b7a2ec002a8daf5cc94c069213ab3862afee9a7c62696e248e5bf474e7ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RescuisineOnRails::Application.config.session_store :cookie_store, key: '_rescuisine-on-rails_session'
